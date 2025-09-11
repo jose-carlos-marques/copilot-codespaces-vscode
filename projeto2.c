d78df54f96cb4e40375d01c51f3e169d213a9049
jose-carlos-marques <marquesjosecaarlos@gmail.com>
2025-09-11 18:07:48 -0300

    Create projeto2.c

---
+++ projeto2.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+int main(){
+
+    int populacao, pontosturisticos;
+    double pib,area;
+    char estado [60];
+    char codigodacarta[60];
+    char nomedacidade[60];
+    float densidadepopulacional, pibpercapita;
+
+
+    printf ("Carta Numero 1 \n");
+
+    printf("Digite o nome da cidade: \n ");
+    scanf ("%59s",  nomedacidade);
+
+    printf ("Digite os Pontos Turisticos da Cidade: \n ");
+    scanf ("%d", &pontosturisticos);
+    
+    printf ("Digite o Estado: \n ");
+    scanf("%59s", estado);
+
+    printf("Digite o numero de populacao da cidade: \n ");
+    scanf ("%d", &populacao);
+
+    printf ("Digite o Pib da cidade: \n ");
+    scanf("%e", &pib);
+
+    printf("Digite a Area da cidade: \n ");
+    scanf("%e", &area);
+ 
+   printf("Digite o Codigo da Carta: \n ");
+   scanf("%59s", &codigodacarta);
+
+   densidadepopulacional = populacao / area;
+   pibpercapita = pib / populacao;
+
+ 
+   printf (" Nome da cidade: %59s",  nomedacidade);
+   printf(" Pontos turisticos: %d", pontosturisticos);
+   printf(" estado: %59s", estado);
+   printf(" populacao: %d", populacao);
+   printf(" pib:", &pib);
+   printf(" area:", &area);
+   printf(" o codigo da carta:", codigodacarta);
+     printf (" ensidade populacional: %f", densidadepopulacional);
+  printf(" O Pib Per Capita: %f", pibpercapita);
+
+
+
+   printf (" Carta Numero Dois \n");
+
+    printf(" Digite o nome da cidade: \n ");
+    scanf ("%59s",  nomedacidade);
+
+    printf (" Digite os Pontos Turisticos da Cidade: \n ");
+    scanf ("%d", &pontosturisticos);
+    
+    printf (" Digite o Estado: ");
+    scanf("%59s", estado);
+
+    printf(" Digite o numero de populacao da cidade: \n ");
+    scanf ("%d", &populacao);
+
+    printf (" Digite o Pib da cidade: \n ");
+    scanf("%e", &pib);
+
+    printf(" Digite a Area da cidade: \n ");
+    scanf("%e", &area);
+ 
+   printf(" Digite o Codigo da Carta: \n ");
+   scanf("%59s", &codigodacarta);
+
+   densidadepopulacional = populacao / area;
+   pibpercapita = pib / populacao;
+
+   printf (" Nome da cidade: %59s ",  nomedacidade);
+   printf(" Pontos turisticos: %d ", pontosturisticos);
+   printf(" estado: %59s ", estado);
+   printf(" populacao: %d ", populacao);
+   printf(" pib: ", &pib);
+   printf(" area: ", &area);
+   printf(" o codigo da carta: ", codigodacarta);
+   printf (" ensidade populacional: %f ", densidadepopulacional);
+  printf(" O Pib Per Capita: %f ", pibpercapita);
+}
